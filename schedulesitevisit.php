06fe185d8f6d651c2253ed3547bfde93dcc39cd3
amrutapawar1 <amrutapawar800@gmail.com>
2025-11-20 13:08:32 +0530

    updated

--- schedulesitevisit.php
+++ schedulesitevisit.php
@@ -29,7 +29,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <no_reply@botmediadigitalmatrketing@gmail.com>' . "\r\n";
+$headers .= 'From: <no_reply@info@botmediadigital.com>' . "\r\n";
 $headers .= 'Cc: ' . "\r\n";
 
 
